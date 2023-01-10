88f0ce767b3dbf674cfd864b5a6aa74dd5c4c769
Thinker Lee <thinker.li@gmail.com>
2023-01-10 08:09:34 -0800

    Update comments in samle_map.bpf.s

--- ex-apps/sample_map.bpf.s
+++ ex-apps/sample_map.bpf.s
@@ -1,5 +1,6 @@
 .section "fentry/__x64_sys_nanosleep"
 .function nanosleep_fentry
+        // Initialize a local variable
         mov.64          r1, 0
         st.w            r10 - 4, r1
         call.helper     14              // bpf_get_current_pid_tgid
@@ -11,8 +12,9 @@
         arsh.64         r1, 32
         jne             r0, r1, @LBB0_2 // goto LBB0_2 if r0 != r1
 
+        // Load the counter value from the map
         ld.dw           r1, @array      // load `array` map
-        // Set up key at a local variable on the stack (default: 0)
+        // Set up key at a local variable on the stack.
         mov.64          r2, r10
         add.64          r2, -4
         call.helper     1               // bpf_map_lookup_elem
@@ -22,6 +24,7 @@
         // Increase the value
         add.64          r1, 1
 
+        // Update map
         mov.64          r3, r10
         add.64          r3, -16
         st.dw           r3, r1
